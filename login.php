23dfe4fcb4efc09f68a0609d01347cae612b32ae
Kevin Zhang <kazp058@gmail.com>
2020-06-30 15:22:48 -0500

    Login error messages implemented

--- login.php
+++ login.php
@@ -7,6 +7,27 @@
     <section class="section-default">
       <h1>Login</h1>
       <?php
+        if(isset($_GET["error"])){
+          if($_GET["error"] == "emptyfields"){
+            echo '<p class="loginerror">Please fill all the fields!</p>';
+          }
+          else if($_GET["error"] == "sqlerror"){
+            echo '<p class="loginerror">There was an error please retry later</p>';
+          }
+          else if($_GET["error"] == "wrongpwd"){
+            echo '<p class="loginerror">Password and username does not match</p>';
+          }
+          else if($_GET["error"] == "nouser"){
+            echo '<p class="loginerror">No username not found, please register if you are a new user</p>';
+          }
+        }
+        else if(isset($_GET["login"])){
+          if($_GET["login"] == "success"){
+            echo '<p class="loginsuccess">Welcome</p>';
+          }
+        }
+      ?>
+      <?php
         if(isset($_SESSION["userId"])){
           echo '<form action="includes/logout.inc.php" method="post"><button type="submit" name="logout-submit">Logout</button></form>';
         }else{
