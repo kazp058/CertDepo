ce4da5eb4259c4173a6c82b6df21a97e4abcf860
kazp058 <kazp058@gmail.com>
2020-08-09 14:40:15 -0500

    Support section started

--- support.php
+++ support.php
@@ -5,13 +5,42 @@
 <main>
    <div class="wrapper-main">
      <section class="section-default">
-        <?php
-         if(isset($_SESSION['userId'])){
+         <?php
+          if(isset($_SESSION['userId'])){
          ?>
-
-        <?php
-        }
-        ?>
+         <div>
+            <a href="request-company-status.php"><p>Request Company Status</p></a>
+            <p>When you request the company status you will not be able to recept certificates, and you will only be able to emit.</p>
+         </div>
+         <?php
+          } else{
+         ?>
+         <div>
+            <a href="login.php"><p>Request Company Status</p></a>
+            <p>When you request the company status you will not be able to recept certificates, and you will only be able to emit.</p>
+            <p>Login to request your company status.</p>
+         </div>
+         <?php
+          }
+         ?>
+        <div>
+           <h2>FAQ</h2>
+           <hr>
+           <ul>
+              <li>
+                 <p>How do we verify the issuers data?</p>
+                 <p>We interview with a representative of the company, to allow the emission of certificates, a code of conduct is signed and if all the legal conditions are met the status of company is granted to an specific account.</p>
+              </li>
+              <li>
+                 <p>How to pay for the emission of certificates?</p>
+                 <p>We accept all debit cards and payment platforms, so that the payment process is smooth as possible.</p>
+              </li>
+              <li>
+                 <p>How to become a company?</p>
+                 <p>Click in the option "request company status" and a member of the support team will contact you.</p>
+              </li>
+           </ul>
+        </div>
      </section>
    </div>
 </main>
