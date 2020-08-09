c41ca826232fc515774f6bfc5e4ed532a9fc9016
kazp058 <kazp058@gmail.com>
2020-08-09 14:39:00 -0500

    Language options started

--- footer.php
+++ footer.php
@@ -1,5 +1,13 @@
-<!DOCTYPE html>
 
-<hmtl>
-
+<footer>
+    <div>
+        <ul>
+            <li><a href="index.php"><p>English</p></a></li>
+            <li><a href="es/index.php"><p>Español</p></a></li>
+        </ul>
+    </div>
+    <div>
+        <p>Created by Kevin Zhang</p>
+    </div>
+</footer>
 </html>
