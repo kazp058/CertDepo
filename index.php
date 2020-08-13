3b30862f7c15107a4574ccda6eccefd4d246f443
kazp058 <kazp058@gmail.com>
2020-08-12 22:41:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <?php
-    require "header.php";
+require "header.php";
 ?>
 
-   <main>
-     <div class="wrapper-main">
-        <section class="section-default">
-          <?php
-              if(isset($_SESSION["userId"])){
-                 ?>
-                 <p class="login-status">You are loggedin!</p>
-                 <?php
-              }
-              else{
-                 ?>
-                 <p class="login-status">You are logged out!</p>
-                 <?php
-              }
-          ?>
-        </section>
-     </div>
-   </main>
+<main>
+   <div class="wrapper-main">
+      <section class="section-default">
+         <?php
+         if (isset($_SESSION["userId"])) {
+         ?>
+            <p class="login-status">You are loggedin!</p>
+         <?php
+         } else {
+         ?>
+            <p class="login-status">You are logged out!</p>
+         <?php
+         }
+         ?>
+      </section>
+   </div>
+</main>
 
 <?php
-    require "footer.php";
+require "footer.php";
 ?>
-
