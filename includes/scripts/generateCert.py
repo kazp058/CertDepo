d37bc8ced911bab31e5c8a2f7daccc8cc9e20179
kazp058 <kazp058@gmail.com>
2020-08-13 13:10:42 -0500

    Update generateCert.py

--- includes/scripts/generateCert.py
+++ includes/scripts/generateCert.py
@@ -22,7 +22,7 @@ line = sys.argv[1:]
 imagename, username, title, token, issuerName, idcert = getparam(line)
 
 if imagename == '001.png':
-    image = Image.open("includes/scripts/certificates/001.png")
+    image = Image.open("includes\\scripts\\certificates\\001.png")
     back_im = image.copy()
 
     qr = qrcode.QRCode(version=1,
@@ -47,4 +47,4 @@ if imagename == '001.png':
     filename = 'Certificado_' + token + '.png'
 
     back_im = back_im.convert('RGB')
-    back_im.save('includes/scripts/certificates/temp/'+filename)
+    back_im.save('includes\\scripts\\certificate\\temp\\'+filename)
