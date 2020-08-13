e9d2d25b121913c0fdb24e02559c3b54ff0dfc74
kazp058 <kazp058@gmail.com>
2020-08-13 05:48:27 -0500

    Python integration

--- includes/scripts/generateCert.py
+++ includes/scripts/generateCert.py
@@ -5,22 +5,23 @@ import string
 import sys
 
 def generateCert(image, name, title, token, issuer):
-    image = Image.open("certificados/" + image)
-    draw = ImageDraw.Draw(image)
+    print("F")
+    #image = Image.open("certificados/" + image)
+    #draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype('tw-cen-mt-6.ttf', size=150)
-    (x, y) = (200,875)
-    color = 'rgb(7,75,114)'
-    draw.text((x, y), name, fill=color, font=font)
-    filename = token + '.pdf'
-    image = image.convert('RGB')
-    image.save(filename)
+    #font = ImageFont.truetype('tw-cen-mt-6.ttf', size=150)
+    #(x, y) = (200,875)
+    #color = 'rgb(7,75,114)'
+    #draw.text((x, y), name, fill=color, font=font)
+    #filename = token + '.pdf'
+    #image = image.convert('RGB')
+    #image.save(filename)
 
-    filename = token + '.jpeg'
-    image = image.convert('RGB')
-    image.save(filename)
+    #filename = token + '.jpeg'
+    #image = image.convert('RGB')
+    #image.save(filename)
 
 line = sys.argv[0]
 line = line.split(',')
 
-generateCert(linea[0], linea[1], linea[2], linea[3], linea[4])
+generateCert(line[0], line[1], line[2], line[3], line[4])
