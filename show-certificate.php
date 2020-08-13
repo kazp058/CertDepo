38fc5a8ddf1d4f6360c33b6c39a6f502f01a3580
kazp058 <kazp058@gmail.com>
2020-08-13 03:58:50 -0500

    Update show-certificate.php

--- show-certificate.php
+++ show-certificate.php
@@ -22,6 +22,7 @@ if (!mysqli_stmt_prepare($stmt, $sql)) {
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
             if ($rowissuer = mysqli_fetch_assoc($result)) {
+                header("show-certificate.php?id=8&F");
                 $command = escapeshellcmd('includes/scripts/generateCert.py 001.png ' . $row['userName'] . ' ' . $row['titleCerts'] . ' ' . $row['tokenCerts'] . ' ' . $rowissuer['issuerName']);
 ?>
                 <p>
