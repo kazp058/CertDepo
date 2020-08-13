21c0d2b700c40cd53dd6b785275134d8881771cf
kazp058 <kazp058@gmail.com>
2020-08-12 22:42:13 -0500

    Update reset-password.php

--- reset-password.php
+++ reset-password.php
@@ -1,38 +1,36 @@
 <?php
-  require "header.php";
+require "header.php";
 ?>
-   <main>
-     <div class="wrapper-main">
-       <section class="section-default">
-          <h1>Reset your password</h1>
-          <p>An e-mail will be send to you with instructions on how to reset your password.</p>
-          <div class='messages'>
-          <?php
-             if(isset($_GET["reset"]))
-             {
-               if($_GET["reset"] == "success")
-               {
-                 echo '<p class="success">Link have been sended to your email!</p>';
-               }
-             }else if(isset($_GET["error"])){
-               if($_GET["error"] == "invalidemail"){
-                 echo '<p class="error">Invalid email</p>';
-               }else if($_GET["error"] == "noaccount"){
-                 echo '<p class="error">No account related to this email was found!</p>';
-               }else if($_GET["error"] == "sqlerror"){
-                 echo '<p class="error">There was a problem, try this later or contact support</p>';
-               }
-             }
-          ?>
-          </div>
-          <form action="includes/reset-request.inc.php" method="post">
-             <input type="text" name="email" placeholder="Enter your e-mail address...">
-             <button class="highlight-button" type="submit" name="reset-request-submit">Recover account</button>
-          </form>
-       </section>
-     </div>
-   </main>
+<main>
+  <div class="wrapper-main">
+    <section class="section-default">
+      <h1>Reset your password</h1>
+      <p>An e-mail will be send to you with instructions on how to reset your password.</p>
+      <div class='messages'>
+        <?php
+        if (isset($_GET["reset"])) {
+          if ($_GET["reset"] == "success") {
+            echo '<p class="success">Link have been sended to your email!</p>';
+          }
+        } else if (isset($_GET["error"])) {
+          if ($_GET["error"] == "invalidemail") {
+            echo '<p class="error">Invalid email</p>';
+          } else if ($_GET["error"] == "noaccount") {
+            echo '<p class="error">No account related to this email was found!</p>';
+          } else if ($_GET["error"] == "sqlerror") {
+            echo '<p class="error">There was a problem, try this later or contact support</p>';
+          }
+        }
+        ?>
+      </div>
+      <form action="includes/reset-request.inc.php" method="post">
+        <input type="text" name="email" placeholder="Enter your e-mail address...">
+        <button class="highlight-button" type="submit" name="reset-request-submit">Recover account</button>
+      </form>
+    </section>
+  </div>
+</main>
 
 <?php
-  require "footer.php";
+require "footer.php";
 ?>
