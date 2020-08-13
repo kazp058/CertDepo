41ef8a7cc79078a7e8cff030235552f98b418a25
kazp058 <kazp058@gmail.com>
2020-08-13 14:02:03 -0500

    Update

--- includes/scripts/generateCert.py
+++ includes/scripts/generateCert.py
@@ -6,18 +6,4 @@ import sys
 import os
 import qrcode
 
-
-def getparam(line):
-    imagename = line[1]
-    username = " ".join(line[3:line.index('-t')])
-    title = " ".join(line[line.index('-t') + 1:line.index('-k')])
-    token = line[line.index('-k')+1]
-    issuerName = " ".join(line[line.index('-i') + 1:line.index('-c')])
-    idcert = line[line.index('-c') + 1]
-    return imagename, username, title, token, issuerName, idcert
-
-
-line = sys.argv[1:]
-
-imagename, username, title, token, issuerName, idcert = getparam(line)
-print(os.getcwd())
+print("F")
--- show-certificate.php
+++ show-certificate.php
@@ -4,32 +4,36 @@ require 'includes/dbh.inc.php';
 $sql = "SELECT * FROM certs WHERE idCerts=?;";
 $stmt = mysqli_stmt_init($conn_certs);
 if (!mysqli_stmt_prepare($stmt, $sql)) {
-    header("Location: ../certificates.php?id=" . $_GET['id'] . "error=sqlerror");
+    header("Location: certificates.php?id=" . $_GET['id'] . "error=sqlerror");
     exit();
 } else {
     mysqli_stmt_bind_param($stmt, "s", $_GET['id']);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
-    if ($row = mysqli_fetch_assoc($result)) {
 
+    if ($row = mysqli_fetch_assoc($result)) {
         $sql = "SELECT * FROM certscompany WHERE certId=?;";
         $stmt = mysqli_stmt_init($conn_certs);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
-            header("Location: ../certificates.php?id=" . $_GET['id'] . "error=sqlerror");
+            header("Location: certificates.php?id=" . $_GET['id'] . "error=sqlerror");
             exit();
         } else {
-            mysqli_stmt_bind_param($stmt, "s", $row['issuerCerts']);
+            mysqli_stmt_bind_param($stmt, "i", $row['issuerCerts']);
             mysqli_stmt_execute($stmt);
             $result = mysqli_stmt_get_result($stmt);
             if ($rowissuer = mysqli_fetch_assoc($result)) {
-                header("show-certificate.php?id=8&F");
-                $command = escapeshellcmd('includes/scripts/generateCert.py 001.png '. $row['userName'] . ' ' . $row['titleCerts'] . ' ' . $row['tokenCerts'] . ' ' . $rowissuer['issuerName']);
-                echo "<p>".shell_exec($command)."</p>";
-?>
                 
-                <img src="<?php echo 'includes/scripts/certificates/temp/'.$row['tokenCerts'].".png";?>" alt="Certificate of <?php echo $row["userName"]?>">
-<?php
+                ?><p><?php echo $rowissuer['issuerCerts'];?></p><?php
+                echo "<p>".shell_exec('python3 includes/scripts/generateCert.py -f 001.png -n '. $row['userName'] . ' -t ' . $row['titleCerts'] . ' -k ' . $row['tokenCerts'] . ' -i ' . $rowissuer['issuerName'] . ' -c '.$row['idCerts'])."</p>";
+                ?>
+                <img src="">
+                <?php
+            }else{
+                header("Location: certificates.php?error=nocert");
+                exit();
             }
         }
     }
 }
+
+require 'footer.php';
