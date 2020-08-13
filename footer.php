8b332340b6e56cff132e141311fb4e6026bfb439
kazp058 <kazp058@gmail.com>
2020-08-13 18:09:04 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,3 @@
-<footer>
-    <div>
-        <ul>
-            <li><a href="index.php">
-                    <p>English</p>
-                </a></li>
-            <li><a href="es/index.php">
-                    <p>Español</p>
-                </a></li>
-        </ul>
-    </div>
-    <div>
-        <p>Created by Kevin Zhang</p>
-    </div>
-</footer>
+
 
 </html>
