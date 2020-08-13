84322649996c176c57cf0f1445be17a02c76df06
Kevin Zhang <kazp058@gmail.com>
2020-08-13 05:39:22 -0500

    a

--- includes/scripts/generateCert.py
+++ includes/scripts/generateCert.py
@@ -1,31 +1,3 @@
 #!/usr/bin/python3.7
-from PIL import Image, ImageDraw, ImageFont
-import string
-import sys
-
-
-def generateCert(image, name, title, token, issuer):
-    image = Image.open("certificados/" + image)
-    draw = ImageDraw.Draw(image)
-
-    font = ImageFont.truetype('tw-cen-mt-6.ttf', size=150)
-    (x, y) = (200,875)
-    color = 'rgb(7,75,114)'
-    draw.text((x, y), name, fill=color, font=font)
-    filename = token + '.pdf'
-    image = image.convert('RGB')
-    image.save(filename)
-
-    filename = token + '.jpeg'
-    image = image.convert('RGB')
-    image.save(filename)
-
 print("F")
 
-image = sys.argv[0]
-name = sys.argv[1]
-title = sys.argv[2]
-token = sys.argv[3]
-issuer = sys.argv[4]
-
-generateCert(image, name, title, token, issuer)
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -37,7 +37,7 @@ if (isset($_POST['signup-submit'])) {
             header("Location: ../signup.php?error=usertaken&mail=" . $email);
             exit();
          } else {
-            $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) Values (?, ?, ?)";
+            $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) Values (?, ?, ?);";
             $stmt = mysqli_stmt_init($conn);
             if (!mysqli_stmt_prepare($stmt, $sql)) {
                header("Location: ../signup.php?error=sqlerror");
