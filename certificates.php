739649ef6b458dbdad7f77c7a703269d9603daa2
kazp058 <kazp058@gmail.com>
2020-08-18 21:29:43 -0500

    Styling

--- certificates.php
+++ certificates.php
@@ -98,8 +98,7 @@ require 'header.php';
             $stmt = mysqli_stmt_init($conn_certs);
 
             if (!mysqli_stmt_prepare($stmt, $sql)) {
-              header("Location: certificates.php?error=sqlerror");
-              exit();
+              
             } else {
               mysqli_stmt_bind_param($stmt, "s", $_SESSION['userId']);
               mysqli_stmt_execute($stmt);
