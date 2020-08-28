1da4ab76a935a5bdf28c1a77bd79b825c1e0f46b
kazp058 <kazp058@gmail.com>
2020-08-27 20:09:20 -0500

    Update messages.php

--- messages.php
+++ messages.php
@@ -4,31 +4,31 @@
       echo '<div class="error"> <h3>Error!</h3>';
       $errormsg = $_GET["error"];
       if ($errormsg == "sql") {
-         echo '<p>There was an error, please retry it later!</p>';
+         echo '<p>Ups! Hubo un error, porfavor intentalo de nuevo.</p>';
       } else if ($errormsg == "nouser") {
-         echo '<p>We couldnt find your account, please log in or create an account</p>';
+         echo '<p>No pudimos encontrar tu cuenta, porfavor crea una o ingresa con una que si exista.</p>';
       } else if ($errormsg == "usertaken") {
-         echo '<p>That email has already been taken please use other email</p>';
+         echo '<p>Ese correo electronico ya esta asociado con otro </p>';
       } else if ($errormsg == "emptyfields") {
-         echo '<p>Please fill in all the fields</p>';
+         echo '<p>Porfavor llena todos los campos.</p>';
       } else if ($errormsg == "invalidmailuid") {
-         echo '<p>The name and the email inserted are not valid!</p>';
+         echo '<p>El nombre y correo ingresado no son validos!</p>';
       } else if ($errormsg == "invalidmail") {
-         echo '<p>The email inserted is not valid!</p>';
+         echo '<p>El correo no es valido!</p>';
       } else if ($errormsg == "invaliduid") {
-         echo '<p>The name inserted is not valid, please only use letters and spaces</p>';
+         echo '<p>El nombre insertado no es valido</p>';
       } else if ($errormsg == "passwordcheck") {
-         echo '<p>The password and the confirmation password do not are the same</p>';
+         echo '<p>La contraseña y la contraseña de verificacion no coinciden</p>';
       } else if ($errormsg == "nocert") {
-         echo '<p>We could not find the certificate, please check the token</p>';
+         echo '<p>No pudimos hallar el certificado</p>';
       } else if ($errormsg == "nospace") {
          echo '<p>There is no more space available, please buy more certificatess</p>';
       } else if ($errormsg == "code") {
-         echo '<p>The claiming code is not the correct one, please check the claiming code</p>';
+         echo '<p>El codigo para reclamar no coincide.</p>';
       } else if ($errormsg == "wrongpwd") {
-         echo '<p>The password and the email does not coincide, please check it and retry it</p>';
+         echo '<p>El correo y la contraseñan no coinciden, porfavor revisa estos datos e intentalo de nuevo.</p>';
       } else if ($errormsg == "nouser") {
-         echo '<p>There is no account linked to that email, please create and account or try to use other email</p>';
+         echo '<p>No hay cuenta registrada con este correo, porfavor crea una cuenta o usa otro correo.</p>';
       } else if ($errormsg == "noitem") {
          echo '<p>There was an error and no certificates where processed, please retry it later or contact support</p>';
       } else if ($errormsg == "notransaction") {
@@ -41,19 +41,19 @@
       echo '<div class="success"> <h3>Success!</h3>';
       $msg = $_GET['success'];
       if ($msg == "addup") {
-         echo '<p>More certificates were assigned!</p>';
+         echo '<p>Se añadieron mas certificados a tu proyecto!</p>';
       } else if ($msg == "cert") {
-         echo '<p>Your data has been sent to the issuer, expect an email with the link to your certificate an a claiming code</p>';
+         echo '<pTus datos fueron enviados, vas a recibir un correo pronto con los datos de tu certificado!</p>';
       } else if ($msg == "created") {
-         echo '<p>Your certificate project has been created</p>';
+         echo '<p>Tu certificado ha sido creado!</p>';
       } else if ($msg == "change") {
-         echo '<p>Your account has a company status and now you can issue certificates</p>';
+         echo '<p>Se le ha asignado a tu cuenta el estado de compañia.</p>';
       } else if ($msg == "payment") {
          echo '<p>You have successfully buyed more certificates!</p>';
       }else if ($msg == "login") {
-         echo '<p>Welcome back! :D</p>';
+         echo '<p>Bienvenido! :D</p>';
       }else if ($msg == "signup") {
-         echo '<p>Your account has been created! Welcome :D</p>';
+         echo '<p>Tu cuenta ha sido creada! Bienvenido :D</p>';
       }
       echo '</div>';
    }
